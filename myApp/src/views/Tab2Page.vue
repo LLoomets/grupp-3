cb0551afedef9328f5fc0566eeef5257dac5b25b
kklaasen1 <kklaasen@tlu.ee>
2025-03-10 14:54:08 +0200

    ios update

--- myApp/src/views/Tab2Page.vue
+++ myApp/src/views/Tab2Page.vue
@@ -8,11 +8,11 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large">Hello world 2</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 2 page" />
+      <ExploreContainer name="Tab 2 page - suvaline" />
     </ion-content>
   </ion-page>
 </template>
