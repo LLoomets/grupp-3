81c7072cbfc845195f9292405b22a993b6182d2f
Liisi Loomets <loomets@tlu.ee>
2025-05-05 15:59:45 +0300

    add search for bars

--- myApp/src/views/Tab2Page.vue
+++ myApp/src/views/Tab2Page.vue
@@ -10,39 +10,43 @@
     </ion-header>
 
     <ion-content class="ion-padding">
-      <l-map style="height: 80vh; width: 100%;" :zoom="13" :center="mapCenter">
-        <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <!-- Otsing -->
+      <ion-input v-model="searchQuery" placeholder="Otsi baari või klubi" debounce="300" clearInput></ion-input>
 
-        <!-- Markerid baaridele ja klubidele -->
-        <l-marker
-          v-for="(place, index) in places"
-          :key="index"
-          :lat-lng="[place.lat, place.lng]"
-        >
-          <l-popup>
-            <div style="display: flex; flex-direction: column; width: 200px; height: 75px;">
-              <strong>{{ place.name }}</strong>
-              <small>{{ place.type }}</small>
-              <ion-button
-                size="small"
-                color="primary"
-                @click="addToBucketlist(place)"
-                :disabled="isPlaceSaved(place)"
-              >
-                {{ isPlaceSaved(place) ? 'Salvestatud' : 'Lisa bucket listi' }}
-              </ion-button>
-            </div>
-          </l-popup>
-        </l-marker>
+      <div style="position: relative; height: 75vh; width: 100%;">
+        <!-- Kaart -->
+        <l-map style="height: 100%; width: 100%;" :zoom="13" :center="mapCenter">
+          <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-        <!-- Kasutaja marker -->
-        <l-marker :lat-lng="userLocation" v-if="userLocation" :icon="userIcon">
-          <l-popup>Sinu asukoht</l-popup>
-        </l-marker>
+          <l-marker v-for="(place, index) in filteredPlaces" :key="index" :lat-lng="[place.lat, place.lng]">
+            <l-popup>
+              <div style="display: flex; flex-direction: column; width: 200px; height: 75px;">
+                <strong>{{ place.name }}</strong>
+                <small>{{ place.type }}</small>
+                <ion-button size="small" color="primary" @click="addToBucketlist(place)"
+                  :disabled="isPlaceSaved(place)">
+                  {{ isPlaceSaved(place) ? 'Salvestatud' : 'Lisa bucket listi' }}
+                </ion-button>
+              </div>
+            </l-popup>
+          </l-marker>
 
-        <!-- Otsinguraadius -->
-        <l-circle v-if="userLocation" :lat-lng="userLocation" :radius="3000" color="hotpink" />
-      </l-map>
+          <l-marker :lat-lng="userLocation" v-if="userLocation" :icon="userIcon">
+            <l-popup>Sinu asukoht</l-popup>
+          </l-marker>
+
+          <l-circle v-if="userLocation" :lat-lng="userLocation" :radius="3000" color="hotpink" />
+        </l-map>
+
+        <!-- otsingu tulemused -->
+        <ion-list v-if="searchQuery && filteredPlaces.length"
+          style="position: absolute; top: 0; left: 0; width: 100%; z-index: 9999; max-height: 70%; overflow-y: auto;">
+          <ion-item v-for="(place, index) in filteredPlaces" :key="'overlay-' + index" button @click="centerMap(place)">
+            {{ place.name }}
+            <ion-note slot="end">{{ place.type }}</ion-note>
+          </ion-item>
+        </ion-list>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -50,12 +54,12 @@
 <script setup lang="ts">
 import {
   IonPage, IonHeader, IonToolbar, IonTitle, IonContent,
-  IonButtons, IonMenuButton
+  IonButtons, IonMenuButton, IonInput, IonItem, IonList, IonNote, IonButton
 } from '@ionic/vue';
 
 import { LMap, LTileLayer, LMarker, LPopup, LCircle } from '@vue-leaflet/vue-leaflet';
 import { Geolocation } from '@capacitor/geolocation';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import L from 'leaflet';
 
 // Kohandatud punane marker
@@ -71,6 +75,19 @@ const userIcon = L.icon({
 const userLocation = ref<[number, number] | null>(null);
 const mapCenter = ref<[number, number]>([59.437, 24.753]);
 const places = ref<any[]>([]);
+const searchQuery = ref('');
+
+const centerMap = (place: any) => {
+  mapCenter.value = [place.lat, place.lng];
+};
+
+const filteredPlaces = computed(() => {
+  const query = searchQuery.value.toLowerCase();
+  if (!query) return places.value;
+  return places.value.filter(place =>
+    place.name.toLowerCase().includes(query)
+  );
+});
 
 onMounted(async () => {
   try {
@@ -111,5 +128,4 @@ const isPlaceSaved = (place: any) => {
   const saved = JSON.parse(localStorage.getItem('bucketlist') || '[]');
   return saved.some((p: any) => p.lat === place.lat && p.lng === place.lng);
 };
-
 </script>
